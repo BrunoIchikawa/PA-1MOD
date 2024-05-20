7ee52498b4b87dde0b1cd3e8dce73c2913962b46
Bruno Santos Ichikawa <149208859+BrunoIchikawa@users.noreply.github.com>
2024-05-20 16:02:17 -0300

    Terminei ate a 10

---
+++ Ex10/Ex10/Ex10/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Digite a idade do atleta:");
+            int idade = int.Parse(Console.ReadLine());
+
+            string categoria;
+
+            if (idade >= 5 && idade <= 10)
+            {
+                categoria = "Infantil";
+            }
+            else if (idade >= 11 && idade <= 15)
+            {
+                categoria = "Juvenil";
+            }
+            else if (idade >= 16 && idade <= 20)
+            {
+                categoria = "Júnior";
+            }
+            else if (idade >= 21 && idade <= 25)
+            {
+                categoria = "Profissional";
+            }
+            else
+            {
+                categoria = "Categoria não encontrada";
+            }
+
+            Console.WriteLine($"O atleta de {idade} anos está na categoria {categoria}.");
+        }
+    }
+
+}
---
+++ Ex8/EX8/EX8/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace EX8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, i, n;
+            n1 = 0;
+            n2 = 1;
+            n = 20;
+
+            Console.Write(n1 + " " + n2 + " ");
+
+
+            for (i = 0; i < n;i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+
+        }
+    }
+}
